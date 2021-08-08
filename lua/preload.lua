0d37579c6b7e439f122f32793f46733d01720f4f
ProditorMagnus <public.ravana@gmail.com>
2021-08-08 16:17:03 +0300

    remove duplicate units from expfreeze, closes #29

--- lua/preload.lua
+++ lua/preload.lua
@@ -14,4 +14,22 @@ function wesnoth.wml_conditionals.EoMa_not_rpg()
 	return not wesnoth.wml_conditionals.EoMa_is_rpg()
 end
 
+function wesnoth.wml_actions.EoMa_remove_array_duplicates(cfg)
+	local name = cfg.name
+	local attribute = cfg.attribute
+
+	local values = {}
+	local inArray = wml.array_access.get(name)
+	local outArray = {}
+	for _, u in pairs(inArray) do
+		if values[u[attribute]] then
+			-- skip duplicate
+		else
+			values[u[attribute]] = true
+			table.insert(outArray, u)
+		end
+	end
+	wml.array_access.set(name, outArray)
+end
+
 -->>
