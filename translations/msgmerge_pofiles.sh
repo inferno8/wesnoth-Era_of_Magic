94bffbfa6952c7907dd3bffee20294af82695d36
inferno8 <inferno-8@wp.pl>
2023-03-17 18:47:35 +0100

    update msgmerge_pofiles.sh

--- translations/msgmerge_pofiles.sh
+++ translations/msgmerge_pofiles.sh
@@ -4,6 +4,6 @@
 # Then, you can run this script
 
 for mylang in de es ga hu it ja pl ru zh_CN; do
-	msgmerge --previous --update --lang=${mylang} wesnoth-Era_of_Magic/${mylang}.po EoMa_translation_template.pot;
-	msgfmt -o wesnoth-Era_of_Magic/${mylang}.mo wesnoth-Era_of_Magic/${mylang}.po;
+	msgmerge --previous --update --lang=${mylang} ${mylang}/LC_MESSAGES/wesnoth-Era_of_Magic.po EoMa_translation_template.pot;
+	msgfmt -o ${mylang}/LC_MESSAGES/wesnoth-Era_of_Magic.mo ${mylang}/LC_MESSAGES/wesnoth-Era_of_Magic.po;
 done
